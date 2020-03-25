dc6dfee591cef507c60d514151480d511f1f9478
soundof5ilence <54803214+soundof5ilence@users.noreply.github.com>
2020-03-25 18:27:47 +0100

    Add files via upload

--- PROJ-1.3-19-20-GD-textadventure-master/js/script.js
+++ PROJ-1.3-19-20-GD-textadventure-master/js/script.js
@@ -5,8 +5,12 @@ const feedback = document.getElementById('feedback');
 const imageLocation = document.getElementById('imageLocation');
 const myDescription = document.getElementById('description');
 const myInventory = document.getElementById('inventory');
+const treasure = document.getElementById('treasure');
 
-let currentLocation = 4;
+
+let gotSword = false;
+let key = false;
+let currentLocation = 15;
 
 let locations = []; // 0 tot 3 zijn geen echte locaties om de map klein te houden.
 locations[0] = "geen locatie";
@@ -17,7 +21,7 @@ locations[4] = "Brood schappen";
 locations[5] = "kaas afdeling";
 locations[6] = "magazijn deur";
 locations[7] = "In het magazijn."
-locations[8] = "melk schappen"
+locations[8] = "zuivel schappen"
 locations[9] = "chips schappen"
 locations[10] ="toilet papier schappen"
 locations[11] = "vlees afdeling";
@@ -42,6 +46,7 @@ images[11] = "room11.jpg";
 images[12] = "room12.jpg";
 images[13] = "room13.jpg";
 images[14] = "room14.jpg";
+images[15] = "room15.jpg";
 
 directions = [];// kamer 0 tot 4 zijn of limits dus is er geen movements daar.
 directions[0] = [];
@@ -50,7 +55,7 @@ directions[2] = [];
 directions[3] = [];
 directions[4] = ["oost","zuid"];
 directions[5] = ["oost", "west", "zuid"];
-directions[6] = ["oost", "west", "zuid"];
+directions[6] = ["oost", "west", "zuid"];//oost komt als je de lightsaber heeft.
 directions[7] = ["west"];
 directions[8] = ["noord", "oost", "zuid"];
 directions[9] = ["noord", "oost", "west", "zuid"];
@@ -61,25 +66,44 @@ directions[13] = ["noord"];
 directions[14] = ["noord", "oost"];
 directions[15] = ["noord", "west"];
 
+lockedDirections = [];
+lockedDirections[6] = ["oost", "west", "zuid"];
 
 descriptions = [];// kamers 0 tot 4 hoor je niet te komen
 descriptions[0] = "how did you get here?";
 descriptions[1] = "how did you get here?";
 descriptions[2] = "how did you get here?";
 descriptions[3] = "how did you get here?";
-descriptions[4] = "u";
-descriptions[5] = "u";
-descriptions[6] = "u";
-descriptions[7] = "u";
-descriptions[8] = "u";
-descriptions[9] = "u";
-descriptions[10] = "u";
-descriptions[11] = "u";
-descriptions[12] = "u";
-descriptions[13] = "u";
-descriptions[14] = "u";
-descriptions[15] = "u";
+descriptions[4] = "je staat bij de broodafdeling, er liggen bolletjes en planken vol brood. niks echt iets bijzonder hier.";
+descriptions[5] = "je staat bij de kaas afdeling. er is nooit te weinig KAAS.";
+descriptions[6] = " je ziet ehh..... en vrouw zittend op een troon van toilet papier voor de magazijn deur. ze zegt: YOU! if you want some of my precious toilet paper you'll need to defeat ME!'....(editer note the key is not functional so i just unlocked the room for now)";
+descriptions[7] = "You have proven yourself worthy. the toilet paper is yours. je staat in het magazijn en vindt een kamer VOL met toilet papier! YOU WON! ";
+descriptions[8] = "je staat in de zuivel afdeling. ik denk niet dat we melk te kort hebben.";
+descriptions[9] = "je staat in de chips schappen. zo te zien met alle jongere die thuis blijven is zowat alles uitverkocht.";
+descriptions[10] = "je staat eindelijk bij het toilet papie- ow er is niks meer over! maar je kan niet gaan zonder.";
+descriptions[11] = "je staat bij de vlees afdeling. de helft van de schappen staan leeg. gelukkig hoef je niks hier te halen.";
+descriptions[12] = "je staat bij de hele kleine diepvries schappen zo te zien is alleen vis nog over.";
+descriptions[13] = "je staat bij een kassa's. er is 1 lege kassa en je ziet een lightsaber liggen op de stoel.";
+descriptions[14] = "je staat in de groente en fruit afdeling. het lijkt dat het corona epidemic niks heeft gedaan tegen het aantal groenten en fruit.";
+descriptions[15] = "je staat in uw lokale albert heijn. en u probeerdt toilet papier halen tegen corona.";
 
+treasures = [];
+treasures[13] = "lightsaber";
+//treasures[6] = "key";
+
+usingtreasures = [];
+usingtreasures[6] = "lightsaber";
+//usingtreasures[6] = "key";
+
+treasureImages = [];
+treasureImages[13] = "lightsaber.png"
+//treasureImages[6] = "Key.png"
+function defeat(){
+if (gotSword = true){
+  lockedDirections[6] = ["oost", "west", "zuid"];
+}
+
+}
 
 myInput.addEventListener('keydown', getInput, false);
 
@@ -104,7 +128,7 @@ function getInput(evt) {
             break;
         }
       } else {
-        feedback.innerHTML = "dat mag niet";
+        feedback.innerHTML = "theres a time and place for everything, but not now and not here.";
         setTimeout(removeFeedback, 2000);
 
       }
@@ -114,25 +138,50 @@ function getInput(evt) {
 
     if (inputArray[0] == "pak") {
       console.log('ga wat pakken');
+      if  ( treasures[currentLocation].indexOf(inputArray[1]) != -1){
+      gotSword = true;
+     console.log('its over anakin...i got the highground.')
+      }
       myInput.value = "";
     }
 
     if (inputArray[0] == "gebruik"){
       console.log('ga wat gebruiken');
+
       myInput.value = "";
     }
 
     if (inputArray[0] != "ga" && inputArray[0] != "pak" && inputArray[0] != "gebruik" ){
-      feedback.innerHTML = "mogelijke commando's zijn: ga, pak, gebruik en help";
+      feedback.innerHTML = "mogelijke commando's zijn: ga, pak en gebruik";
       myInput.value = "";
       setTimeout(removeFeedback, 4000);
     }
-
+     
+    
   }
 }
 
+
+function showMyTreasure(){
+  if(typeof treasureImages[currentLocation] != "undefined")
+  {
+    console.log(treasureImages[currentLocation]);
+    treasure.src = "Pictures/" + treasureImages[currentLocation];
+  }
+}
+function Locked(a) {
+  if (lockedDirections[a] == null){
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+
 function giveLocation() {
-  divLocation.innerHTML = locations[currentLocation];
+  
+  divLocation.innerHTML = locations[currentLocation]+ " => grid " + currentLocation;
   myDescription.innerHTML = descriptions[currentLocation];
   imageLocation.src = "media/" + images[currentLocation];
   myDirections = "mogelijke richtingen zijn: ";
@@ -140,7 +189,8 @@ function giveLocation() {
     myDirections += "<li>" + directions[currentLocation][i] + "</li>";
   }
   myPossibilities.innerHTML = myDirections;
-  myInventory.innerHTML = "uw inventory is leeg";
+  myInventory.innerHTML = "schrijf pak en dan wat je wilt oppakken.";
+  showMyTreasure(currentLocation);
 }
 
 function removeFeedback() {
